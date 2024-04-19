631ba57a59869e15714d83707d28f5a87d534bd9
Mike Lines <mike@lastminute.com>
2024-04-19 11:36:22 +0200

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+import gulp from 'gulp';
+import zip from 'gulp-zip';
+import fs from 'fs/promises';
+
+let manifest = JSON.parse(await fs.readFile("manifest.json"));
+let currentVersion = manifest.version;
+
+function build(callback) {
+    gulp
+        .src(["src/**", "_locales/**", "manifest.json", "README.md", "LICENSE", "favicon-maestro.png"], { base: "." })
+        .pipe(zip("maestro-extension-." + currentVersion + ".zip"))
+        .pipe(gulp.dest("deployments"));
+
+    callback();
+}
+
+gulp.task("build", build);
