ac0ec5967c6d13fb50abdf4c1a77e68269b6c155
Mike Lines <mike@lastminute.com>
2024-04-24 09:12:05 +0200

    fix effectsactive icon, update popup look & feel

--- src/inject/js/overlay.js
+++ src/inject/js/overlay.js
@@ -351,7 +351,7 @@ class OverlayApp extends Globals {
                 lightning = document.getElementById('effectsActiveIcon');
             }
 
-            if ((autoFogEnabled && autoFogOnActivityPeak || autoFogOnTimer) || autoEffectsEnabled || autoStrobeEnabled) {
+            if ((autoFogEnabled && (autoFogOnActivityPeak || autoFogOnTimer)) || autoEffectsEnabled || autoStrobeEnabled) {
                 if (lightning.innerHTML === '') {
                     lightning.innerHTML = '<span class="tooltip" data-toggle="tooltip" data-placement="top" title="Auto programs active"><svg width="26" height="26" title="Auto programs active" stroke="currentColor" style="animation: 0.525s ease-in-out 0s infinite normal none running strobeAnimation;" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M315.27 33 96 304h128l-31.51 173.23a2.36 2.36 0 0 0 2.33 2.77h0a2.36 2.36 0 0 0 1.89-.95L416 208H288l31.66-173.25a2.45 2.45 0 0 0-2.44-2.75h0a2.42 2.42 0 0 0-1.95 1z"></path></svg></span>';
                 }
