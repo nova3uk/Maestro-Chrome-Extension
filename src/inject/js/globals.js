fe3cfcf461a67dae703a3d8026f8b07ede83f13e
Mike Lines <mike@lastminute.com>
2024-04-28 20:38:35 +0200

    styles

--- src/inject/js/globals.js
+++ src/inject/js/globals.js
@@ -248,6 +248,11 @@ class Globals {
     formatDate = (d, withSeconds = false) => {
         return d.getFullYear().toString() + "-" + ((d.getMonth() + 1).toString().length == 2 ? (d.getMonth() + 1).toString() : "0" + (d.getMonth() + 1).toString()) + "-" + (d.getDate().toString().length == 2 ? d.getDate().toString() : "0" + d.getDate().toString()) + " " + (d.getHours().toString().length == 2 ? d.getHours().toString() : "0" + d.getHours().toString()) + ":" + ((parseInt(d.getMinutes() / 5) * 5).toString().length == 2 ? (parseInt(d.getMinutes() / 5) * 5).toString() : "0" + (parseInt(d.getMinutes() / 5) * 5).toString()) + (withSeconds ? ":" + (d.getSeconds().toString().length == 2 ? d.getSeconds().toString() : "0" + d.getSeconds().toString()) : "");
     }
+    getScaledValue = (value, sourceRangeMin, sourceRangeMax, targetRangeMin, targetRangeMax) => {
+        var targetRange = targetRangeMax - targetRangeMin;
+        var sourceRange = sourceRangeMax - sourceRangeMin;
+        return (value - sourceRangeMin) * targetRange / sourceRange + targetRangeMin;
+    }
     getObjectDiff = (obj1, obj2) => {
         const diff = Object.keys(obj1).reduce((result, key) => {
             if (!obj2.hasOwnProperty(key)) {
--- src/inject/js/settings.js
+++ src/inject/js/settings.js
@@ -52,6 +52,7 @@ class SettingsApp extends Globals {
             await this.getBrightness().then(() => {
                 let val = Math.floor(this.brightness.value * 255);
                 document.getElementById('master_dimmer').value = val;
+                maestro.SettingsApp.setDimmerValue(val);
             });
         }, 60000);
 
@@ -62,7 +63,15 @@ class SettingsApp extends Globals {
             });
         }, 1000);
     };
-
+    setDimmerValue = async (value) => {
+        let brightness = maestro.SettingsApp.getScaledValue(value, 0, 255, 50, 100) + '%'
+        if (value == 0) {
+            document.getElementById('masterDimmerVal').innerHTML = `${Math.floor(Math.round((value / 255) * 100))}% <svg xmlns="http://www.w3.org/2000/svg" style="position:relative;top:-2px" width="16" height="16" fill="currentColor" class="bi bi-lightbulb-off-fill" viewBox="0 0 16 16"><path d="M2 6c0-.572.08-1.125.23-1.65l8.558 8.559A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a2 2 0 0 0-.453-.618A5.98 5.98 0 0 1 2 6m10.303 4.181L3.818 1.697a6 6 0 0 1 8.484 8.484zM5 14.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5M2.354 1.646a.5.5 0 1 0-.708.708l12 12a.5.5 0 0 0 .708-.708z"/></svg>`;
+        } else {
+            document.getElementById('masterDimmerVal').innerHTML = `${Math.floor(Math.round((value / 255) * 100))}% <svg xmlns="http://www.w3.org/2000/svg" style="position:relative;top:-2px" width="16" height="16" fill="currentColor" class="bi bi-lightbulb-fill" viewBox="0 0 16 16"><path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a2 2 0 0 0-.453-.618A5.98 5.98 0 0 1 2 6m3 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5"/></svg>`;
+        }
+        document.getElementById('masterDimmerVal').style.filter = "brightness(" + brightness + ")";
+    }
     audioLevelHdlr = async (audioLevel) => {
         this.logging = true;
 
@@ -2981,21 +2990,15 @@ class SettingsApp extends Globals {
         await this.getBrightness().then(() => {
             let val = Math.floor(this.brightness.value * 255);
             document.getElementById('master_dimmer').value = val;
+            maestro.SettingsApp.setDimmerValue(val);
         });
 
         document.getElementById('master_dimmer').addEventListener('change', function (ele) {
             maestro.SettingsApp.setMasterDimmer(this.value);
         });
-
-        // //group dimmers initial setting
-        // $('input[name="groupDimmer"]').each(function (index, ele) {
-        //     let group = activeFixtureGroups.find(group => group.name == ele.dataset.group);
-        //     for (let fixtureId of group.fixtureId) {
-        //         let fixture = activeStage.fixture.find(ele => ele.id == fixtureId);
-        //         let channels = fixture.attribute.filter(channel => channel.type === "DIMMER" || channel.type === "MASTER_DIMMER")
-        //             .map((channel) => ({ index: fixture.attribute.indexOf(channel), channel }));
-        //     }
-        // });
+        document.getElementById('master_dimmer').addEventListener('input', function (ele) {
+            maestro.SettingsApp.setDimmerValue(this.value);
+        })
     };
     setMasterDimmer = async (value) => {
         let url = `${maestro.SettingsApp.maestroUrl}api/${this.apiVersion}/brightness`;
